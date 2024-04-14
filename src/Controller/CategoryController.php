812fc4efb0214e8a42df45156327abcdb43989da
Alexandre-st <alexandre.saintprix@gmail.com>
2024-04-14 22:39:33 +0200

    Add the product detail page

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,12 +14,22 @@ class CategoryController extends AbstractController
     #[Route('/{category}', name: 'app_category_category')]
     public function getProductsFromCategory(string $category, CategoryRepository $categoryRepository): Response
     {
-        $category = $categoryRepository->findOneBy(['name' => $category]);
+        $category = $categoryRepository->findOneBy(['name' => $category]); // dump and die (dd) the products of the category
         $products = $category->getProducts();
-
+        
         return $this->render('category/index.html.twig', [
             'category' => $category,
             'products' => $products,
+        ]);
+    }
+    
+    #[Route('/{category}/{products}', name: 'app_category_category_produit')]
+    public function getProducts(string $products, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy(['slug' => $products]);
+
+        return $this->render('product/index.html.twig', [
+            'product' => $product,
         ]);
     }
 }
