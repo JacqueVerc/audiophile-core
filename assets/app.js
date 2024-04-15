a86735da8b28e2308cf72e75ed5ff52cb710f928
Alexandre-st <alexandre.saintprix@gmail.com>
2024-04-15 22:24:06 +0200

    Upgrade home

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,20 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.scss';
+import "./styles/app.scss";
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
 
-const headerMenu = document.querySelector('.header-menu');
+// const headerMenu = document.querySelector('.header-menu');
 
-headerMenu.addEventListener('click', () => {
-  console.log('header menu clicked');
+// headerMenu.addEventListener('click', () => {
+//   console.log('header menu clicked');
+// });
+
+const menu = document.querySelector(".header-menu");
+const showMenu = document.querySelector(".header-menu-bg");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  showMenu.classList.toggle("active");
 });
