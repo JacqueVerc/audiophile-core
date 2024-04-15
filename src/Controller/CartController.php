7cd9485cc060019033ac3a6808e0b8f8e5c2ff75
JacqueVerc <jacquesvercucque@gmail.com>
2024-04-16 00:08:34 +0200

    Feat: ajout fixtures sur cart/cartLines et récupération d un cart

---
+++ src/Controller/CartController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CartLineRepository;
+use App\Repository\CartRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CartController extends AbstractController
+{
+    #[Route('/cart', name: 'app_cart')]
+    public function index(UserRepository $userRepository, CartRepository $cartRepository, CartLineRepository $cartLineRepository): Response
+    {
+        $cart = $cartRepository->findOneBy(['user' => $this->getUser()]);
+
+        $cartLines = $cartLineRepository->findBy(['cart' => $cart]);
+
+//        Attention CartLines est un tableau d'entité
+        return $this->render('cart/index.html.twig', [
+            'cartLines' => $cartLines
+        ]);
+    }
+}
