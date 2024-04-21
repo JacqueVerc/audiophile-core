e545e6a4fb3c2602cfbb12750190a01c98f91a4c
JacqueVerc <jacquesvercucque@gmail.com>
2024-04-21 17:33:08 +0200

    Ajout gestion commande

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\CommandLine;
 use App\Entity\CustomerAdress;
+use App\Entity\Order;
 use App\Form\CustomerAddressFormType;
 use App\Form\RegistrationFormType;
+use App\Processor\CartProcessor;
+use App\Repository\CartLineRepository;
+use App\Repository\CartRepository;
 use App\Repository\CustomerAdressRepository;
+use App\Repository\OrderRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,28 +21,78 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/order', name: 'app_order')]
 class OrderController extends AbstractController
 {
-    #[Route('/validate', name: 'app_order_validate')]
-    public function getUserAdress(CustomerAdressRepository $customerAdressRepository, Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/order/validate', name: 'app_order_validate')]
+    public function userAdressForm(CartProcessor $cartProcessor, CustomerAdressRepository $customerAdressRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $customer = $customerAdressRepository->findOneBy(['user' => $this->getUser()]);
+        $isConfirmed = $request->query->get('isConfirmed');
         $customerForm = new CustomerAdress();
         $form = $this->createForm(CustomerAddressFormType::class, $customerForm);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $oldAdress = $customerAdressRepository->findOneBy(['user' => $this->getUser()]);
+            if ($oldAdress) {
+                $entityManager->remove($oldAdress);
+            }
             $customerForm->setUser($this->getUser());
 
             $entityManager->persist($customerForm);
 
             $entityManager->flush();
-        }
 
-        return $this->render('order/index.html.twig', [
+            $customerForm = new CustomerAdress();
+            $form = $this->createForm(CustomerAddressFormType::class, $customerForm);
+        }
+        $customer = $customerAdressRepository->findOneBy(['user' => $this->getUser()]);
+
+        $res = array_merge(
+            $cartProcessor->getCart($this->getUser()),
+            [
             'customerAddressForm' => $form,
             'customer' => $customer,
-        ]);
+            'isConfirmed' => $isConfirmed,
+            ]
+        );
+
+        return $this->render('order/index.html.twig', $res);
+    }
+
+    #[Route('/order/confirm', name: 'app_order_confirm')]
+    public function confirmOrder(UserRepository $userRepository, OrderRepository $orderRepository, CartRepository $cartRepository, EntityManagerInterface $manager): Response
+    {
+        $order = new Order();
+
+        $cart = $cartRepository->findOneBy(['user' => $this->getUser()]);
+        foreach ($cart->getCartLines() as $cartLine){
+            $commandLine = new CommandLine();
+            $commandLine->setQuantity($cartLine->getQuantity());
+            $commandLine->setProduct($cartLine->getProduct());
+            $commandLine->setOrders($order);
+
+            $manager->remove($cartLine);
+
+            $manager->persist($commandLine);
+        }
+        $orderNumer = count($orderRepository->findAll()) + 1;
+
+        $order->setUser($this->getUser());
+        $order->setDate(new \DateTime());
+        $order->setOrderNumber($orderNumer);
+        $order->setValid(true);
+
+        $manager->persist($order);
+
+        $manager->remove($cart);
+
+        $newCart = new Cart();
+        $newCart->setUser($userRepository->findOneBy(['id' => $this->getUser()]));
+
+        $manager->flush();
+
+        $manager->persist($newCart);
+        $manager->flush();
+        return $this->redirectToRoute('app_order_validate', ['isConfirmed' => true]);
     }
 }
